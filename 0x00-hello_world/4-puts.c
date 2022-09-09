ef9616cae05f289c4b7995c498055c5c03463598
Gloire Chabu <cortesgloire@gmail.com>
2022-09-09 07:00:55 -0700

    Hot fix

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+
+
+int main ()
+{
+  char str[53];
+  strcpy(str, "Programming is like building a multilingual puzzle \n");
+  puts(str);
+  return (0);
+
+}
