010baa8af981be1553584380af14c107bc614b67
Gloire Chabu <cortesgloire@gmail.com>
2022-09-09 08:00:00 -0700

    Hot fix

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,8 +5,7 @@
  * main function is a Simple program to write to std out a message
  * Return: 0 if the program runs without errors
 */
-
-int main(void) 
+int main(void)
 {
 	char str1[52];
 	strcpy(str1, "Programming is like building a multilingual puzzle\n");
