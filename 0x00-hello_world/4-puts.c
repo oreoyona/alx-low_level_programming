cc141289309e61cd61f9d225b57ad5e697b18b7f
Gloire Chabu <cortesgloire@gmail.com>
2022-09-09 07:04:38 -0700

    Hot fix

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 #include <string.h>
+/*
+ *A Simple program to write to std out a message
+*/
 
-
-int main ()
+int main(void)
 {
-  char str[53];
-  strcpy(str, "Programming is like building a multilingual puzzle \n");
+  char str[52];
+  strcpy(str, "Programming is like building a multilingual puzzle\n");
   puts(str);
   return (0);
 
