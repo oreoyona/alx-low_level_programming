025c3d8dce3494a83158b88a16346276ea5f5a57
Gloire Chabu <cortesgloire@gmail.com>
2022-09-09 07:34:12 -0700

    Hot fix

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 
 /**
+ * main - this a main function
  * print a simple message
+ * Return zero
  */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return(0);
+	return (0);
 }
