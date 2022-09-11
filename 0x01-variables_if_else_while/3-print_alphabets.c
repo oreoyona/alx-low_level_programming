81803fba05815a13eb4be220dd4fcf51af570075
Gloire Chabu <cortesgloire@gmail.com>
2022-09-10 18:29:10 -0700

    Hot fix

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - prints out the alphabet
+ * Return: 0 if the program runs succesfully
+ */
+int main(void)
+{
+	char alphabet[26] = "abcdefghijklmnopqrstuvwxyz";
+	char alphaBET[26] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	int i = 0;
+
+	for(i = 0; i < 26; ++i)
+		putchar(alphabet[i]);
+	for(i = 0; i < 26; ++i)
+		putchar(alphaBET[i]);
+	putchar("/n");
+	return (0);
+}
