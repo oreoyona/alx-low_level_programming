fe0a53a7c947d31f8f385d5e46abfe194a137b47
Gloire Chabu <cortesgloire@gmail.com>
2022-09-10 18:43:47 -0700

    Hot fix

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -12,7 +12,7 @@ int main(void)
 
 	for (i = 0; i < 26 ; ++i)
 	{
-		if(alphabet[i] != 'e' && alphabet[i] != 'q')
+		if (alphabet[i] != 'e' && alphabet[i] != 'q')
 			putchar(alphabet[i]);
 	}
 	putchar('\n');
