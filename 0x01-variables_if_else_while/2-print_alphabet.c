09d83b35072b26112f38dc05d6e9b25bf5840854
Gloire Chabu <cortesgloire@gmail.com>
2022-09-10 17:42:30 -0700

    Hot fix

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -9,6 +9,9 @@ int main(void)
 {
 	char alphabet[26] = "abcdefghijklmnopqrstuvwxyz";
 	int ch, i, n = 0;
+
+	while((ch = getchar()) != EOF && n < 100)
+		alphabet[n++] = ch;
 	
 	for(i = 0; i < n ; ++i)
 		putchar(alphabet[i]);
