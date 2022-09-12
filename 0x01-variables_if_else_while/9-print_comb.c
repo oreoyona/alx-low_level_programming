765501b7f32dc21541114a371904e272d70e5668
Gloire Chabu <cortesgloire@gmail.com>
2022-09-12 10:04:35 -0700

    Hot fix

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 /**
- * main - prints all the letters of the alphabet
- * Return: 0 uppon success and 1 otherwise
+ * main - prints the first digits
+ * Return: 0 upon success and 1 otherwise
  */
 int main(void)
 {
 	int i = 0;
-	
+
 	for (i = 0; i < 10; i++)
 	{
 		putchar(i + '0');
@@ -19,4 +19,3 @@ int main(void)
 	putchar('\n');
 	return (0);
 }
-
