a4c0f207001c094039d8688bae8a15b6711e9a47
Gloire Chabu <cortesgloire@gmail.com>
2022-09-12 10:39:30 -0700

    Hot fix

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - prints the first 100 digits
+ * Return: 0 uppon success and 1 otherwise
+ */
+int main(void)
+{
+	int num1, num2;
+	for (num1 = 0; num1 < 10; num1++)
+	{
+		for (num2 = 0; num2 < 10; num2++)
+		{
+			putchar((num1 % 10) + '0');
+			putchar((num2 % 10) + '0');
+
+			if (num1 == 9 && num2 == 9)
+				continue;
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
