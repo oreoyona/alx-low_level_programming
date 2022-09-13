447ea2d0278d5ba7081e55a5c8056b4680e4b031
Gloire Chabu <cortesgloire@gmail.com>
2022-09-13 08:55:07 -0700

    Hot fix

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _islower - tells if the given letter is lowercase or not
+ * @c: the character of type int to be checked
+ * Return: 1 if c is lowercase
+ * 0 otherwise
+ */
+int _islower(int c)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		if (c == letter)
+		{
+			return (1);
+		}
+		else
+			return (0);
+	}
+}
