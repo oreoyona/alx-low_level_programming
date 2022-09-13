f9096851463b1096d8269edee44c58937a0e4227
Gloire Chabu <cortesgloire@gmail.com>
2022-09-13 02:39:19 -0700

    Hot fix

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,14 +1,15 @@
-#include <main.h>
+#include <stdio.h>
+#include "main.h"
 /**
  * main - prints a string to stdo
  * Return: 0 upon success, 1 otherwise
  */
 int main(void)
 {
-	char word[7] = "_putchar";
+	char word[8] = "_putchar";
 	int i = 0;
 
-	for (i = 0; i < 7; i++)
+	for (i = 0; i < 8; i++)
 	{
 		putchar(word[i]);
 	}
