a6f7850c4faea3b9e81b3e79832f257b11e1bec2
Gloire Chabu <cortesgloire@gmail.com>
2022-09-13 08:33:14 -0700

    Hot fix

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print 10x the alphabet in lower_case
+ * Return:0 upon success, 1 otherwise
+ */
+void print_alphabet_x10(void)
+{
+	char letter;
+	int x = 0;
+
+	while (x < 10)
+	{
+		x++;
+		for (letter = 'a'; letter < 'z'; letter++)
+		{
+			_putchar(letter);
+		}
+		_putchar('\n');
+	}
