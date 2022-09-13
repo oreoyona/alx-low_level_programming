24a3a8d9317f01e5fa742e7b162a03b1f151be84
Gloire Chabu <cortesgloire@gmail.com>
2022-09-13 03:19:42 -0700

    Hot fix

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,3 +1,5 @@
+#include "main.h"
+
 /**
  * print_alphabet - print the alphabet in lower_case
  * Return:0 upon success, 1 otherwise
@@ -8,7 +10,7 @@ void print_alphabet(void)
 
 	for (letter = 'a'; letter < 'z'; letter++)
 	{
-		putchar(letter);
+		_putchar(letter);
 	}
-	putchar('\n');
+	_putchar('\n');
 }
