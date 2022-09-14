c3996865a1e8f213d4d6acf58d674d123b82a187
Gloire Chabu <cortesgloire@gmail.com>
2022-09-14 07:27:39 -0700

    Hot fix

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -2,8 +2,8 @@
 
 /**
  * add - add 2 numbers
- * @num1 - the first number
- * @num2 - the second number
+ * @num1: the first number
+ * @num2: the second number
  * Return: the sum of the 2 given numbers
  */
 int add(int num1, int num2)
