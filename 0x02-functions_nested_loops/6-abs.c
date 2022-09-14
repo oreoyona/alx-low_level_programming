60c54bb636d7256116fd2cf69669468045e344a0
Gloire Chabu <cortesgloire@gmail.com>
2022-09-14 05:29:31 -0700

    Hot fix

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,5 +1,5 @@
 #include "main.h"
-#include <math.h>
+
 /**
  * _abs - prints the absolute value of a number
  * @n: the number to be computed
@@ -7,8 +7,7 @@
  */
 int _abs(int n)
 {
-	int db = n * n;
-	int rc = sqrt(db);
-
-	return rc;
+	if (n < 0)
+		n = -n;
+	return n;
 }
