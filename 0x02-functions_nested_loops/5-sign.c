36d47de1ed34330fba5e3b0216c40b20541c1fd0
Gloire Chabu <cortesgloire@gmail.com>
2022-09-14 04:51:44 -0700

    Hot fix

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_sign - tells wether a given number is positive or not
+ * @n: the number to be tested
+ * Return: 1 if positive
+ * 0 if 0
+ * -1 f negative
+ */
+int print_sign(int n)
+{
+	int code = -1;
+
+	if (n == 0)
+	{
+		code = 0;
+		_putchar('0');
+	}
+	else if ( n > 0)
+	{
+		code = 1;
+		_putchar('+');
+	}
+	else
+		_putchar('-');
+	return code;
+}
