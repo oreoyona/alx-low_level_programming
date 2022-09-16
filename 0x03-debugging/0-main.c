6782d12576871a2ac847c04deb2e88f69bb75634
Gloire Chabu <cortesgloire@gmail.com>
2022-09-16 14:21:49 -0700

    Hot fix

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * man - tests functions
+ * Return: 0;
+ */
+int main(void)
+{
+	int = 0;
+
+	i = 98;
+	positive_or_negative(i);
+	return (0);
+}
---
+++ 0x03-debugging/main.h
@@ -0,0 +1,6 @@
+#include <unistd.h>
+/**
+ * contans all the prototypes
+ */
+
+void positive_or_negative(int i);
