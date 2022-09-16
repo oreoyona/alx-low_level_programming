944735c0baeb4803b70f818b9cd298ff7709aa9b
Gloire Chabu <cortesgloire@gmail.com>
2022-09-16 06:42:41 -0700

    Hot fix

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_line - print a new line to std outpout
+ * @n: the number of line to be printed
+ * Return: void
+ */
+int print_line(int n)
+{
+	int count = 0;
+
+	if (n <= 0)
+		putchar('');
+	else
+	{
+		while (count < n)
+		{
+			count++;
+			_putchar('_');
+		}
+	}
+}
