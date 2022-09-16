378a5f602ef03ed67f098422a039de9eba98804f
Gloire Chabu <cortesgloire@gmail.com>
2022-09-16 08:20:19 -0700

    Hot fix

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * fizzbuzz - print the 100 first digit
+ * all multiple of 3 are replaced by fizzbuzz
+ * Return: void
+ */
+void fizzbuzz(void)
+{
+	int x = 0;
+
+	for (x = 0; x < 100; x++)
+	{
+		if (x % 3 == 0)
+			printf("Fizz");
+		else if (x % 5 == 0)
+			printf("Buzz");
+		else if (x % 3 == 0 && x % 5 == 0)
+			printf("FizzBuzz");
+		printf("%d", x);
+	}
+}
