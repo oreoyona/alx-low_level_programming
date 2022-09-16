449d94cfe684358eb25345121abc3d76d96d8e0c
Gloire Chabu <cortesgloire@gmail.com>
2022-09-15 19:54:20 -0700

    Hot fix

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints the numbers < 9
+ * Return: void
+ */
+void print_numbers(void)
+{
+	int i = 0;
+
+	for (i = 0; i < 10; i++)
+	{
+		_putchar(i + '0');
+	}
+}
