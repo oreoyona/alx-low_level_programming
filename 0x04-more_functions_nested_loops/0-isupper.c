4278183b0f640dafb39879133eaa5b2224f0c93a
Gloire Chabu <cortesgloire@gmail.com>
2022-09-15 21:35:01 -0700

    Hot fix

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * _isupper - tests weather a char is in uppercase
- * @c: the char to be tested
+ * _isupper - Tests weather a char is in uppercase
+ * @c: The char to be tested
  * Return: 1 if c is in uppercase
  * 0 if not
  */
