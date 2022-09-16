eb42912b01bd758a9a946c63f22d9f491fd38695
Gloire Chabu <cortesgloire@gmail.com>
2022-09-15 19:23:38 -0700

    Hot fix

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isdigit - tests wether a given cher is a digit
+ * @c: thechar to be tested
+ * Return: 1 if char is a digit, 0 otherwise
+ */
+int _isdigit(int c)
+{
+	int i = 0;
+	int code = 0;
+
+	for (i = 0; i <= 9; i++)
+	{
+		if (c == i)
+			code = 1;
+	}
+	return (code);
+}
