27482018c611e6937861cedc65fcd517d98b70e7
Gloire Chabu <cortesgloire@gmail.com>
2022-09-16 08:39:15 -0700

    Hot fix

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,12 +11,12 @@ void fizzbuzz(void)
 
 	for (x = 1; x < 100; x++)
 	{
-		if (x % 3 == 0)
+		if (x % 3 == 0 && x % 5 != 0)
 		{
 			printf(" Fizz ");
 			continue;
 		}
-		else if (x % 5 == 0)
+		else if (x % 5 == 0 && x % 3 != 0)
 		{
 			printf(" Buzz ");
 			continue;
