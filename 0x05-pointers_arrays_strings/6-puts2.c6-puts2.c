5adb9d0c23de83cf81afc9933864e78f5b6745b8
Gloire Chabu <cortesgloire@gmail.com>
2022-09-19 12:21:25 -0700

    Hot fix

---
+++ 0x05-pointers_arrays_strings/6-puts2.c6-puts2.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * puts2 - prints every other character of a string, starting with the first character
+ * followed by a new line
+ * @str: the string we will be working on
+ * Retun : void
+ */
+void puts2(char *str)
+{
+	int i;
+	char x;
+
+	for (i = 0; str[i] != 0; i++)
+	{
+		if (i % 2 == 0)
+		{
+			x = str[i];
+			_putchar(1);
+		}
+	}
+	_putchar('\n');
+}
