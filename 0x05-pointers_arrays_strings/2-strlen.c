6046eb66db098e4a5dba6d9183406ab1409d4140
Gloire Chabu <cortesgloire@gmail.com>
2022-09-19 11:34:12 -0700

    Hot fix

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _strlen - returns the length of a given string
+ * @s: the string to work on
+ * Return: the lenght of the string
+ */
+int _strlen(char *s)
+{
+	int x;
+
+	for (x = 0; s[x] != 0; x++)
+	{
+	}
+	return (x);
+}
