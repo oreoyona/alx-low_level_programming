e3720b25474a85bfeb0e3c57f9ca93e330fda962
Gloire Chabu <cortesgloire@gmail.com>
2022-09-19 12:31:28 -0700

    Hot fix

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,6 +1,7 @@
 #include "main.h"
 /**
- * puts2 - prints every other character of a string, starting with the first character
+ * puts2 - prints every other character of a string
+ * starting with the first character
  * followed by a new line
  * @str: the string we will be working on
  * Retun : void
