abe72e4ec573a0489aff9978df3b19b7e722194a
Gloire Chabu <cortesgloire@gmail.com>
2022-09-19 11:54:15 -0700

    Hot fix

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_rev - prints a string in reverse followed by a new line
+ * @s: the string we will work on
+ * Return: void
+ */
+void print_rev(char *s)
+{
+	int i;
+
+	for (i = 0; str[i] != 0; i++)
+	{
+	}
+	for (i = i - 1; i >= 0; i--)
+	{
+		_putchar(s[c]);
+	}
+	_putchar('\n');
+}
