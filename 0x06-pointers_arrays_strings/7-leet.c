344c0e90d2bf6dc012a824fffbe0a50e1d2ee767
Gloire Chabu <cortesgloire@gmail.com>
2022-09-22 08:03:12 -0700

    Hot fix

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * leet - this function encodes a string in 1337
+ * @s: the string we will be working on
+ * Return: the string encoded
+ */
+char *leet(char *s)
+{
+	int x;
+	int y;
+	char *chars1 = "aAeEoOtTlL";
+	char *chars2 = "4433007711";
+
+	for (x = 0; s[x] != '\0'; x++)
+	{
+		for (y = 0; y < 10; y++)
+		{
+			if (s[x] == chars1[x])
+				s[x] = chars2[y];
+		}
+	}
+	return (s);
+}
