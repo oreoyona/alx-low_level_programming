6c4d7193019d958f79f085cf2e1bc5cb6cf854e5
Gloire Chabu <cortesgloire@gmail.com>
2022-09-22 08:33:51 -0700

    Hot fix

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * rot13 - encodes @s into rot13
+ * @s: the string we will be working on
+ * Return: encoded string
+ */
+char *rot13(char *)
+{
+	int x;
+	int y;
+
+	char chars1[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char chars2[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (x = 0; s[x] != '\0'; x++)
+	{
+		for (y = 0; chars1[y] != '\0'; y++)
+		{
+			if (s[x] == chars1[y])
+			{
+				s[x] = chars2[y];
+				break;
+			}
+		}
+	}
+	return (s);
+}
