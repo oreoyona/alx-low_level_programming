c98cefc2d56a92bf82bc4a83c341fe5f4079467a
Gloire Chabu <cortesgloire@gmail.com>
2022-09-22 08:50:21 -0700

    Hot fix

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -6,16 +6,19 @@
  */
 char *_strcat(char *dest, char *src)
 {
-	int x, y = 0;
+	int x = 0;
+	int y = 0;
 
 	while (dest[x] != '\0')
 		x++;
+
 	while (src[y] != '\0')
 	{
 		dest[x] == src[y];
 		x++;
 		y++;
 	}
+
 	dest[x] = '\0';
 	return (dest);
 }
