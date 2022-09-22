b103edf20c95da26cac6ea59fb02b597716984e1
Gloire Chabu <cortesgloire@gmail.com>
2022-09-22 07:09:25 -0700

    Hot fix

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * reverse_array - a function that 
+ * reverses the content of an array of integers
+ * @a: the array
+ * @n: the length of the array
+ */
+void reverse_array(int *a, int n)
+{
+	int i = 0;
+	int demi;
+
+	for (demi = n / 2; demi > 0; demi--; i++)
+	{
+		a[n - i - 1] += a[i];
+		a[i] = a[n - i - 1] - a[i];
+		a[n - i - 1] = a[n - i - 1] - a[i];
+	}
+}
