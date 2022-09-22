70184d07979ede50a82fb837eebe1d396ef95efe
Gloire Chabu <cortesgloire@gmail.com>
2022-09-22 06:51:51 -0700

    Hot fix

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -9,7 +9,7 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int count;
 
-	for (count = 0; count < n && src[count] != '\0'; i++)
+	for (count = 0; count < n && src[count] != '\0'; count++)
 	{
 		dest[count] = src[count];
 	}
