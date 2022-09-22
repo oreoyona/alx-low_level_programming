c3952eae4a27ff65449aee66593abbf998e64d95
Gloire Chabu <cortesgloire@gmail.com>
2022-09-22 05:30:00 -0700

    Hot fix

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _strncpy - copies a string
+ * @dest: first string
+ * @src: second string
+ * @n: number of copies
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i, l = 0;
+
+	while(a != n)
+	{
+		dest[l] = src[i];
+		l++;
+		i++;
+		if (src[i] == '\0')
+			break;
+	}
+	while (b != n)
+		dest[l++] = '\0';
+	return (dest);
+}
