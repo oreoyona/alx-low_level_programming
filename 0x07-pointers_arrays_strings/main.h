ad21736a521524997aed9c7d5e7a9b764afdfa25
Gloire Chabu <cortesgloire@gmail.com>
2022-09-26 05:03:41 -0700

    Hot fix

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,5 +1,9 @@
 #include <unistd.h>
 /**
- * main - contains some prototypes
+ * _putchar - mimics the putchar function of std library
+ * @c: the char to be printed
+ * Return: 1 on success
+ * -1 otherwise
  */
+int _putchar(char c);
 char *_memset(char *s, char b, unsigned int n);
