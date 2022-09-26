5bfc73d204603bb97acaaeadd03b1e620d48434a
Gloire Chabu <cortesgloire@gmail.com>
2022-09-26 05:23:24 -0700

    Created the second file inside of which I created a fuction that mimics the functions of the memcpy functionof the std lib

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _memcpy -  copies memory area
+ * @dest: where we will copy
+ * @src: what we will be copying
+ * @n: the number of byte to copy
+ * Return: a pointer
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int x;
+
+	for (x = 0; x < n; x++)
+	{
+		src[n] = dest;
+	}
+	return (dest);
+}
