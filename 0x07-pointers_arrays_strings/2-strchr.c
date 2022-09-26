a752a7b058be6b3dd9f45b59f421624d51a84f2f
Gloire Chabu <cortesgloire@gmail.com>
2022-09-26 05:52:23 -0700

    Created the second file

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _strchr - locates a character in a string
+ * @s:the string
+ * @c: the char
+ * Return: a pointer
+ */
+char *_strchr(char *s, char c)
+{
+	int x;
+	
+
+	for (x = 0; x < s[x]; x++)
+	{
+		if (s[x] == c)
+			return (s[x]);
+	}
+}
