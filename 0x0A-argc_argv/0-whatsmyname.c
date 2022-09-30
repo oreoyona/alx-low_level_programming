c0ffbee7030ad5d657c47f523e9d1d31d0e0e02c
Gloire Chabu <cortesgloire@gmail.com>
2022-09-30 07:55:02 -0700

    Hot fix

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -3,11 +3,11 @@
 
 /**
  * main - prints the name of the program
- * @argc - number of arguments
- * arguments of the program
+ * @argc: number of arguments
+ * @argc: arguments of the program
  */
-int main(char *argv[])
+int main(int argc, char *argv[])
 {
 	printf("%s\n", argv[0]);
-	return (0);
+	return (argc);
 }
