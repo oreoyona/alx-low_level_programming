c96a88772769efe4e8bb9ed696d2f182409cbc83
Gloire Chabu <cortesgloire@gmail.com>
2022-10-05 07:24:56 -0700

    Hot fix

--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -6,9 +6,15 @@
  * Return: always 0 upon success
  */
 int _putchar(char c);
+/**
+ * isInteger - checks if a given string is an int
+ * @s: the string to be checked
+ * Return: 0 uppon success
+ */
 int isInteger(const char *s)
 {
 	int x = 0;
+
 	while (s[x] != '\0')
 	{
 		if (s[x] < '0' || s[x] > '9')
