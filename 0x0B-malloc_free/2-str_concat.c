cb5f9fdc6d2d11ad88620118c740b127e6be8fa9
Gloire Chabu <cortesgloire@gmail.com>
2022-10-07 02:51:21 -0700

    Hot fix

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -10,11 +10,10 @@ char *str_concat(char *s1, char *s2)
 {
 	unsigned int x;
 	unsigned int l;
-	char *a;
+	char *a = s1;
 
 	for (x = 0; s1[x] != '\0'; x++)
 	{
-		a[x] = s1[x];
 	}
 	for (l = 0; s2[l] != '\0'; x++, l++)
 	{
