ea661d3f689ea184590316f59bef70c9daeb42cc
Gloire Chabu <cortesgloire@gmail.com>
2022-10-07 02:35:25 -0700

    Hot fix

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include <stdlib.h>
 /**
- * _strdup - returns a pointer to a newly allocated space in memory, 
+ * _strdup - returns a pointer to a newly allocated space in memory
  * which contains a copy of the string given as a parameter.
  * @str: the string we will be working on
  * Return: a pointer
