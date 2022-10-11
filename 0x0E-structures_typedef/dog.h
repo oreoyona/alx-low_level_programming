eaee63ad001266d4fdc4188e8a433110d3017932
Gloire Chabu <cortesgloire@gmail.com>
2022-10-11 03:45:25 -0700

    Hot fix

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,9 +2,8 @@
  * struct dog - here we define a dog
  * @name: the name of the dog
  * @age: the age of the dog
- * @owner: the owner of the dog
- *
- * The dog struct describes the elements 
+ * @owner: the owner of the do
+ * The dog struct describes the elements
  * that must be present to every single dog in our program
  */
 struct dog
