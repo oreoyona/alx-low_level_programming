7f3d34b33f224f497adf4b6e287263f25ba79b80
Gloire Chabu <cortesgloire@gmail.com>
2022-10-11 05:37:01 -0700

    Hot fix

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -10,7 +10,7 @@ int main(void)
 {
 	dog_t *gDog;
 
-	gDog = new_dog("Ruby", 7, "Gloire");
+	gDog = new_dog("Ruby", 7.1, "Gloire");
 	printf("My name is %s, and I am %.1f :) - Woof!\n", gDog->name, gDog->age);
 	return (0);
 }
