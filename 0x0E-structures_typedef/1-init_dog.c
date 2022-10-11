719fd45f25e4ba622a4566b02f2df1fbdf380571
Gloire Chabu <cortesgloire@gmail.com>
2022-10-11 04:53:49 -0700

    Hot fix

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -5,11 +5,15 @@
  * @name: the name
  * @age: age
  * @owner: the owner
+ * Return: void
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	(*d).name = name;
-	(*d).age = age;
-	(*d).owner = owner;
+	if (d != NULL)
+	{
+		(*d).name = name;
+		(*d).age = age;
+		(*d).owner = owner;
+	}
 }
 
