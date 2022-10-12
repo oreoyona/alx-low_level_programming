47980aae755a6d96132073b509f68369cb6e7301
Gloire Chabu <cortesgloire@gmail.com>
2022-10-12 03:52:09 -0700

    Hot fix

--- 0x0F-function_pointers/main.h
+++ 0x0F-function_pointers/main.h
@@ -1,4 +1,14 @@
-#include <unistd.h>
+#ifndef MAIN_H
+#define MAIN_H
 
+#include <unistd.h>
+/**
+ * _putchar - mimics putchar
+ * @c: the char to be trated
+ * Return: an int
+ */
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
+
+
+#endif
