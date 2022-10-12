2ca26de2e1db1aa2dd491c7018067b39e3adc355
Gloire Chabu <cortesgloire@gmail.com>
2022-10-12 14:31:49 -0700

    Hot fix

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,19 @@
+#include "function_pointers.h"
+/**
+ * array_iterator - executes a function given as a parameter
+ * on each element of an array
+ * @array: the array
+ * @size: of the array
+ * @action: the 3rd param
+ * Return: void
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t x = 0;
+
+	while (x < size)
+	{
+		action(array[x]);
+		x++;
+	}
+}
---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,22 @@
+#include "function_pointers.h"
+/**
+ * int_index - searches for an integer.
+ * @array: the array we will work on
+ * @size: of the array
+ * @cmp: a pointer
+ * Return: an int
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int x = 0;
+
+	if (array == NULL || cmp == NULL || size <= 0)
+		return (-1);
+	while (x < size)
+	{
+		if (cmp(array[i]))
+			return (x);
+		x++;
+	}
+	return (-1);
+}
