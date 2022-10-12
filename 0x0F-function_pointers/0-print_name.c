c3d971f2f16bfede769492ec1f81ddba5420a911
Gloire Chabu <cortesgloire@gmail.com>
2022-10-12 03:59:31 -0700

    Hot fix

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -3,7 +3,7 @@
  * print_name - prints the name
  * @name: the name to be printed
  * @f: the function that will actually print the name
- * Return (0);
+ * Return: void
  */
 void print_name(char *name, void (*f)(char *))
 {
