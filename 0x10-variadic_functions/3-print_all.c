d2a531fea229957c0a96ac6f5e2d9d554dfc8ce5
Gloire Chabu <cortesgloire@gmail.com>
2022-10-14 09:15:15 -0700

    Hot fix

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -33,7 +33,7 @@ void print_string(va_list arg)
 
 	if (str == NULL)
 	{
-		print("(nil)");
+		printf("(nil)");
 		return;
 	}
 	printf("%s", str);
@@ -71,56 +71,5 @@ void print_all(const char * const format, ...)
 	}
 	printf("\n");
 
-	va_end(args);
-}
-/**
- * print_string - prints a string
- * @arg: arguments
- */
-void print_string(va_list arg)
-{
-	char *str;
-	str = va_arg(arg, char *);
-
-	if (str == NULL)
-	{
-		printf("(nil)");
-		return;
-	}
-	printf("%s", str);
-}
-/**
- * print_all -prints anything followed by a new line
- * @format: argument types.
- * @...: arguments
- */
-void print_all(const char * const format, ...)
-{
-	va_list args;
-	int i = 0, j = 0;
-	char *separator = "";
-	printer_t funcs[] = {
-		
-		{"c", print_char},
-		{"i", print_int},
-		{"f", print_float},
-		{"s", print_string}
-	};
-	
-	va_start(args, format);
-	while (format && (*(format + i)))
-	{
-		j = 0;
-		while (j < 4 && (*(format + i) != *(funcs[j].symbol)))
-			j++;
-		if (j < 4)
-		{
-			printf("%s", separator);
-			funcs[j].print(args);
-			separator = ", ";
-		}
-		i++;
-	}
-	printf("\n");
 	va_end(args);
 }
