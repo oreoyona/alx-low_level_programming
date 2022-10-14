7f28b7ed7735c918ec2d89b1724e95657fd3d38e
Gloire Chabu <cortesgloire@gmail.com>
2022-10-14 09:17:03 -0700

    Hot fix

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,13 +1,6 @@
 #include "variadic_functions.h"
 #include <stdio.h>
 #include <stdarg.h>
-
-void print_char(va_list arg);
-void print_int(va_list arg);
-void print_float(va_list arg);
-void print_string(va_list arg);
-void print_all(const char * const format, ...);
-
 /**
  * print_char - prints a char
  * @arg: va_list of args
