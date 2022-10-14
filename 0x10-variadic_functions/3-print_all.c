004a03ae1789364d94374e69285fb6bf87553bcd
Gloire Chabu <cortesgloire@gmail.com>
2022-10-14 12:25:48 -0700

    Hot fix

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -2,67 +2,48 @@
 #include <stdio.h>
 #include <stdarg.h>
 /**
- * print_char - prints a char
- * @arg: va_list of args
- */
-void print_char(va_list arg)
-{
-	char x;
-
-	x = va_arg(arg, int);
-	printf("%c", x);
-}
-
-/**
- * print_string - prints a string
- * @arg: args
- */
-
-void print_string(va_list arg)
-{
-	char *str;
-
-	str = va_arg(arg, char *);
-
-	if (str == NULL)
-	{
-		printf("(nil)");
-		return;
-	}
-	printf("%s", str);
-}
-/**
- * print_all - prints anything followed by a new line
- * @format: format
- * @...: arguments
+ * print_all - prints anything
+ * @format: list of types of arguments passed to the function
  */
 void print_all(const char * const format, ...)
 {
-	va_list args;
-	int i = 0, j = 0;
-	char *separator = "";
-	printer_t funcs[] = {
-		{"c", print_char},
-		{"i", print_int},
-		{"f", print_float},
-		{"s", print_string}
-	};
-	va_start(args, format);
+	int x = 0;
+	char *str, *sprtr = "";
 
-	while (format && (*(format + i)))
+	va_list y;
+
+	va_start(y, format);
+
+	if (format)
 	{
-		j = 0;
-		while (j < 4 && (*(format + i) != *(funcs[j].symbol)))
-			j++;
-		if (j < 4)
+		while (format[x])
 		{
-			printf("%s", separator);
-			funcs[j].print(args);
-			separator = ", ";
+			switch (format[x])
+			{
+			case 'c':
+				printf("%s%c", sprtr, va_arg(y, int));
+				break;
+			case 'i':
+				printf("%s%d", sprtr, va_arg(y, int));
+				break;
+			case 'f':
+				printf("%s%f", sprtr, va_arg(y, double));
+				break;
+			case 's':
+				str = va_arg(y, char *);
+				if (!str)
+					str = "(nil)";
+				printf("%s%s", sprtr, str);
+				break;
+			default:
+				x++;
+				continue;
+			}
+			sprtr = ", ";
+			x++;
 		}
-		i++;
 	}
-	printf("\n");
 
-	va_end(args);
+	printf("\n");
+	va_end(y);
 }
