04c8606dcf313cd5af38de10416bbc31ac5930d8
Gloire Chabu <cortesgloire@gmail.com>
2022-10-20 05:01:31 -0700

    created the first function

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -11,13 +11,13 @@ size_t print_list(const list_t *h)
 	size_t x = 0;
 	const list_t *head = h;
 	const list_t *runner = h->next;
-
-	if (h->str == NULL)
-		printf("%s\n", "[0] (nil)");
 	while (head)
 	{
-		printf("%s\n", head->str);
-		x++;
+		printf("[%u] ", head->len);
+		if (head->str == NULL)
+			printf("(nil)\n");
+		else
+			printf("%s\n", head->str);
 		head = runner;
 	}
 	return (x);
