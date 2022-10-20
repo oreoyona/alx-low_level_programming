2bcb56368dc8a842943ad0da52976c5bc5a12250
Gloire Chabu <cortesgloire@gmail.com>
2022-10-20 04:27:06 -0700

    corrected betty

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -4,14 +4,15 @@
 /**
  * print_list - counts the number of nodes
  * @h: the linked list
+ * Return: number of nodes
  */
 size_t print_list(const list_t *h)
 {
 	size_t x = 0;
 	const list_t *head = h;
-	const list_t *runner = h -> next;
+	const list_t *runner = h->next;
 
-	if (h -> str == NULL)
+	if (h->str == NULL)
 		printf("%s\n", "[0] (nil)");
 	while (head)
 	{
