fdae0662b2cd211b5c1ebddd05e044d6e9300554
Gloire Chabu <cortesgloire@gmail.com>
2022-10-20 06:00:01 -0700

    created the first function

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * list_len - printts the number of nodes of a linked list
+ * @h: head of linked list
+ * Return: the number of nodes
+ */
+size_t list_len(const list_t *h)
+{
+	size_t  count = 0;
+
+	while (h)
+	{
+		count++;
+		h = h->next;
+	}
+	return (count);
+}
