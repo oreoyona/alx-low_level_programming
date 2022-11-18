7e3dc495006c29dfe80b526c005b4e94f04dc06d
Gloire Chabu <cortesgloire@gmail.com>
2022-11-18 04:09:41 -0800

    hot fix

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -8,15 +8,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int safe_close(int)
-{
-	int error;
-
-	error = close(description);
-	if (error < 0)
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", description);
-	return (error);
-}
 size_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
