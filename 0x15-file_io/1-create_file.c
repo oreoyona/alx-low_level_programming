bca47af1dc46c76047e9ace8ddc1921a8f6478e9
Gloire Chabu <cortesgloire@gmail.com>
2022-11-18 04:15:53 -0800

    hot fix

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,11 +1,10 @@
-#include "main."
+#include "main.h"
 /**
- * create_file - Creates a file.
- * @filename: A pointer to the name of the file to create.
- * @text_content: A pointer to a string to write to the file.
+ * create_file - Creates a file
+ * @filename: name of the file to create.
+ * @text_content: content
  *
- * Return: If the function fails - -1.
- *         Otherwise - 1.
+ * Return: an int
  */
 int create_file(const char *filename, char *text_content)
 {
