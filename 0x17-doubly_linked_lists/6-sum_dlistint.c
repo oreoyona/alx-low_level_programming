068908025bcb0d6c4004799efa8dc751b76cdd03
Gloire Chabu <cortesgloire@gmail.com>
2022-12-12 14:47:09 -0800

    hot fix

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -1,9 +1,7 @@
 #include "lists.h"
-
 /**
- * sum_dlistint - returns the sum of all the data (n)
- * of a doubly linked list
- *
+ * sum_dlistint - returns the sum of all the data
+ * in a doubly linked list
  * @head: head of the list
  * Return: sum of the data
  */
