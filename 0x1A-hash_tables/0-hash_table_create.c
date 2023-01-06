76b6887320d062761337fbcbf7a30d7306fcf7e9
Gloire Chabu <cortesgloire@gmail.com>
2023-01-06 03:38:21 -0800

    Hot fix

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -12,7 +12,9 @@ hash_table_t *hash_table_create(unsigned long int size)
 
 	ht = malloc(sizeof(hash_table_t));
 	if (ht == NULL)
+	{
 		return (NULL);
+	}
 	ht->size = size;
 	ht->array = malloc(size * sizeof(hash_node_t *));
 	if (ht->array == NULL)
