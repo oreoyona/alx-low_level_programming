d9c62c2806092913fb40f694e4adb5f1992e2aa9
Gloire Chabu <cortesgloire@gmail.com>
2023-01-08 03:07:24 -0800

    Hot fix

--- exercices/structs.c
+++ exercices/structs.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #define TRUE 3
 #define D_SIZE 3
 /**
@@ -11,7 +12,7 @@
 typedef struct person{
 	char *name;
 	int age;
-	char *f[D_SIZE];
+	char *f;
 }person;
 
 /**
@@ -22,16 +23,58 @@ typedef struct person{
  */
 typedef struct p_node{
 	int id;
-	person content;
+	person *content;
 	struct p_node *next;
-}pnt;
+}p_node;
+/**
+ * gen - generates a unique id
+ * @str: the name of the person
+ * Return: the id
+ */
+int gen(char *str)
+{
+	int i, length, code;
 
+	length = strlen(str);
+	code = 0;
+
+	for (i = 0; i < length; i++)
+		code += str[i];
+	return (code);
+}
+
+/** 
+ * init - creates a ll of 1 node
+ * @head: pointer to the head of the ll
+ * Return: p_node
+ */
+p_node *init(person *ob)
+{
+	p_node *head;
+	
+	if(!ob)
+		return (NULL);
+	head->id = gen(ob->name);
+	head->content = NULL;
+	head->next = NULL;
+	
+	return (head);
+}
 /**
  * main - entry point of the program
  * Return: void
  */
 int main()
 {
+	person *gloire;
+	p_node *head;
+
+	gloire->name="Gloir";
+	gloire->age=26;
+	gloire->f = "A B C ";
+	head = init(gloire);
+	if (head) printf("Success\n");
+	else printf("Failed\n");
 
 	return (TRUE);
 }
