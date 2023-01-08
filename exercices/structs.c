6369230b50d6d55efe0408725ca2fab6d806f50c
Gloire Chabu <cortesgloire@gmail.com>
2023-01-07 17:33:53 -0800

    Hot fix

--- exercices/structs.c
+++ exercices/structs.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#define TRUE 3
 #define D_SIZE 3
 /**
  * person - object to built a person
@@ -22,7 +23,7 @@ typedef struct person{
 typedef struct p_node{
 	int id;
 	person content;
-	p_node *next;
+	struct p_node *next;
 }pnt;
 
 /**
@@ -31,38 +32,6 @@ typedef struct p_node{
  */
 int main()
 {
-	int i;
-	pnt *h, *h1, *runner;
-	person *gloire, *meghan;
-	gloire->name = "Gloir";
-	gloire->age = 26;
-	gloire->f[0] = "A";
-	gloire->f[1] = "B";
-	gloire->f[2] = "C";
 
-	meghan->name = "Megha";
-	meghan->age = 41;
-	meghan->f[0] = "D";
-	meghan->f[1] = "E";
-	meghan->f[2] = "F";
-	
-
-	h->id = 1;
-	h->content = *gloire;
-	h->next = NULL;
-
-	h1->id = 2;
-	h1->content = *meghan;
-	h1->next = h;
-	
-	runner = h1;
-
-	while(runner != NULL)
-	{
-		printf("[%s->id:%d]\n", runner->content->name, runner->id);
-		runner = runner->next;
-		i++;
-	}
-	printf("Nombre de noeuds:%d\n", i);
-	return (0);
+	return (TRUE);
 }
